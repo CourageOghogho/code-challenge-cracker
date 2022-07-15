c961ab2791cb078e4fab0a431f0d15f7491154d3
Courage Oghogho <oghogho.courage@gmail.com>
2022-07-15 15:12:20 +0100

    typo errors removed web

--- algorithm/challenge-5.js
+++ algorithm/challenge-5.js
@@ -1,17 +1,17 @@
-var alphabests=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphabets=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 function caesarCifer(str, num){
     let result="";
     let splittedStr = str.split("");
     let targetIndex=0;
     for(let i=0; i<splittedStr.length; i++){
-        let originalIndex=alphabests.indexOf(splittedStr[i])
+        let originalIndex=alphabets.indexOf(splittedStr[i])
        if(originalIndex+num>25){
         targetIndex=originalIndex+num-26
        }
        else{
         targetIndex=originalIndex+num
        }
-       result+=alphabests[targetIndex]
+       result+=alphabets[targetIndex]
     }
     return result;
 }
